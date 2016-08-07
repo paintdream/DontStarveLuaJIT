3e4f0b446d440e07761a04187749393a602f7d9d
paintdream <paintdream@paintdream.com>
2016-08-08 01:25:30 +0800

    VERSION INFO

--- src/luajit/src/lua.h
+++ src/luajit/src/lua.h
@@ -16,8 +16,8 @@
 #include "luaconf.h"
 
 
-#define LUA_VERSION	"Lua 5.1"
-#define LUA_RELEASE	"Lua 5.1.4 (LuaJIT MikePall, Don't Starve adapted by PaintDream)"
+#define LUA_VERSION	"Lua 5.1.4 (LuaJIT by Mike Pall, Don't Starve Adapted by PaintDream)"
+#define LUA_RELEASE	"Lua 5.1.4 (LuaJIT by Mike Pall, Don't Starve Adapted by PaintDream)"
 #define LUA_VERSION_NUM	501
 #define LUA_COPYRIGHT	"Copyright (C) 1994-2008 Lua.org, PUC-Rio"
 #define LUA_AUTHORS	"R. Ierusalimschy, L. H. de Figueiredo & W. Celes"
