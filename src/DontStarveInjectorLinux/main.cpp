2c29443e79164e90e52a30a628eb20b7c971563d
paintdream <paintdream@paintdream.com>
2020-04-09 20:40:07 +0800

    Unhook luaopen_io on both server and client.

--- src/DontStarveInjectorLinux/main.cpp
+++ src/DontStarveInjectorLinux/main.cpp
@@ -290,7 +290,7 @@ public:
 
 			const char* funcs[] = {
 				"luaL_loadfile", "luaL_newstate", "luaL_optinteger",
-				"luaL_optlstring", "luaL_optnumber", 
+				"luaL_optlstring", "luaL_optnumber", "luaopen_io", 
 			};
 
 			for (size_t i = 0; i < sizeof(funcs) / sizeof(funcs[0]); i++) {
